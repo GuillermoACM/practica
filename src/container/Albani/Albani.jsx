fdb049c59a2125d83c33db9d3804798ed8e3a227
akgodoy <138242404+akgodoy@users.noreply.github.com>
2023-07-11 10:21:42 -0430

    Cambios Albani

--- src/container/Albani/Albani.jsx
+++ src/container/Albani/Albani.jsx
@@ -9,9 +9,42 @@ const Albani = () => {
 			<div className="app__albani-title">
 			    <h1>Albani</h1>
 			</div>
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			<div className="app__albani-excerpt">
-				<p className="p-text"></p>
+			
+			<div>
+			<ul> 
+			<a href="#"> 
+				<li>La Familia Manson, o simplemente La Familia, fue un grupo o secta criminal establecida en California, a finales de los años 1960, dirigida por Charles Manson. Ganaron notoriedad después del asesinato de la actriz Sharon Tate y otras cuatro personas el 9 de agosto de 1969, a manos de Tex Watson y otros tres miembros de «La Familia Manson», actuando bajo las instrucciones de Charles Manson.  </li>
+				</a>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Manson1968.jpg/250px-Manson1968.jpg"/> 
+				
+				
+				<li>Ku Klux Klan (KKK) es un grupo de odio3​ supremacista blanco estadounidense, conocido por promover por medio de actos violentos y propagandísticos el racismo, la xenofobia y el antisemitismo, así como la homofobia, el anticatolicismo y el anticomunismo</li>
+				
+				<img src="https://www.es.amnesty.org/fileadmin/user_upload/AP9511250962_KKK_racismo.jpg"/>
+				
+				<li> Dragon Ball Z (ドラゴンボールZ Doragon Bōru Zetto?) es una serie de manga japonesa producida por Toei Animation. Es el segundo producto de la franquicia Dragon Ball, es la secuela de la serie de anime de 1986 Dragon Ball y adapta los últimos 325 capítulos de la serie de manga Dragon Ball creada por Akira Toriyama, que se publicó en Weekly Shōnen Jump de 1988 a 1995. </li>
+			</ul>
+			<img src="https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/36bdc5ea4443cd8e42f22ec7d3884cbb.jpe"/> 
 			</div>
+			</div>
+			
+			
+			
+			
+			
+			
+			
+			
 		</div>
     </div>
   )
