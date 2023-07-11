67e18ff153a57a613cc1b3284778173decb24123
Miguelpers <137926338+Miguelpers@users.noreply.github.com>
2023-07-11 17:11:11 -0430

    cambio a la seccion de curiosidades

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,22 @@
 import esquema01 from '../assets/esquema01.png';
 import esquema02 from '../assets/esquema02.png';
+import carro from '../assets/carro.jpg';
+import piramides from '../assets/piramides.jpg';
+import castillo from '../assets/castillo.jpg';
+import estrellamar from '../assets/estrella-mar.jpg';
+import molino from '../assets/molino.jpg';
+import perro from '../assets/perro.jpg';
+
 
 const images = {
   esquema01,
-	esquema02,
+  esquema02,
+  carro,
+  piramides,
+  castillo,
+  estrellamar,
+  molino,
+  perro,
 }
 
 export default images; 
--- src/container/Miguel/Miguel.jsx
+++ src/container/Miguel/Miguel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import './Miguel.scss';
-
+import { images } from '../../constants';
 const Miguel = () => {
   return (
     <div className="app__miguel">
@@ -14,21 +14,93 @@ const Miguel = () => {
 				
 				
 				
-				<div className="section">
+				<div className="section d-flex flex-row flex-wrap justify-content-center container">
 				
-							<div className="article-header"></div>	
-								<div className="body-article">
-									<div className="article-card">
-										<h3 className="title-article">POR QUÉ LOS PERROS NO PUEDEN COMER CHOCOLATE</h3>
-									</div>
-										<p>Los perros no pueden metabolizar la teobromina, un compuesto químico presente en el chocolate, por lo que esta se acumula rápidamente hasta alcanzar niveles tóxicos y puede provocar su muerte.
-		</p>
-										<p>Los perros al ser incapaces de metabolizar la teobromina (molécula que se encuentra en las semillas de cacao, las nueces de cola y el té) pueden padecer un conjunto de afecciones que van desde daño cardiaco y el sistema nervioso hasta la muerte.
-		</p>
-										<p>Esta molécula puede afecta a los seres humanos, pero la dosis, a diferencia de los perros, debe ser muy elevada; aunado a esto, el ser humano puede metabolizar dicha sustancia, evitando con esto los efectos adversos provocado por la misma. </p>
-									<a className="link"> Ver articulo.</a>
-								</div>
 							
+					<div className="card mt-3 mb-3 col-11 bg-success  " >
+					  <div className="row g-0">
+						<div className="col-md-4">
+						  <img src={images.carro} className="img-fluid rounded-start" alt="carro"/>
+						</div>
+						<div className="col-md-8">
+						  <div className="card-body">
+						
+							<p className="card-text text-start">Conducir un coche sucio en Rusia está prohibido. Si bien es de sentido común mantener limpio el coche para mejorar tu visibilidad, en este país la policía dictamina cuál es el nivel de suciedad sancionable, siendo más alto si no se ve bien la matrícula.</p>
+							
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div className="card mt-3 mb-3 col-11 bg-success  " >
+					  <div className="row g-0">
+						<div className="col-md-4">
+						  <img src={images.piramides} className="img-fluid rounded-start" alt="carro"/>
+						</div>
+						<div className="col-md-8">
+						  <div className="card-body">
+						
+							<p className="card-text text-start">Sudán es el país con más pirámides que Egipto. Cuenta con 255 de estas edificaciones, las cuales fueron construidas entre los años 1070 a.C y 350 d.C, mientras que Egipto solo tiene 138.</p>
+							
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div className="card mt-3 mb-3 col-11 bg-success  " >
+					  <div className="row g-0">
+						<div className="col-md-4">
+						  <img src={images.castillo} className="img-fluid rounded-start" alt="carro"/>
+						</div>
+						<div className="col-md-8">
+						  <div className="card-body">
+						
+							<p className="card-text text-start">Al-Khazneh (El tesoro de Petra), es una construcción a mitad del desierto en Jordania. Tiene casi 12 pisos de altura. Su nombre, ‘tesoro’, es por una simple y sencilla razón: en el pasado se creía que en este imponente lugar se guardaban todo tipo de tesoros. Hasta la fecha, esos tesoros nunca se encontraron.</p>
+							
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div className="card mt-3 mb-3 col-11 bg-success  " >
+					  <div className="row g-0">
+						<div className="col-md-4">
+						  <img src={images.estrellamar} className="img-fluid rounded-start" alt="carro"/>
+						</div>
+						<div className="col-md-8">
+						  <div className="card-body">
+						
+							<p className="card-text text-start">Las sanguijuelas son hermafroditas con 32 cerebros, nueve pares de testículos y una mandíbula con tres hileras de 100 dientes cada una.</p>
+							
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div className="card mt-3 mb-3 col-11 bg-success  " >
+					  <div className="row g-0">
+						<div className="col-md-4">
+						  <img src={images.molino} className="img-fluid rounded-start" alt="carro"/>
+						</div>
+						<div className="col-md-8">
+						  <div className="card-body">
+						
+							<p className="card-text text-start">Los molinos de viento siempre giran al contrario de las manecillas del reloj, excepto en Irlanda. Si las aspas están giradas hacia un lado (de la vertical hacia la derecha), el molino girará en sentido contrario a las agujas del reloj. Este tipo de construcción de aspas lo podemos ver en la mayoría de los molinos del mundo. Sin embargo, en Irlanda, ese ángulo de inclinación de las aspas de sus molinos, es la contraria (de la vertical hacia la izquierda) y por eso giran para el otro lado.</p>
+							
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div className="card mt-3 mb-3 col-11 bg-success  " >
+					  <div className="row g-0">
+						<div className="col-md-4">
+						  <img src={images.perro} className="img-fluid rounded-start" alt="carro"/>
+						</div>
+						<div className="col-md-8">
+						  <div className="card-body">
+						
+							<p className="card-text text-start">Cada año, desde el año 2000, se celebra el concurso ‘perros más feos del mundo’, en California, Estados Unidos. El objetivo del concurso no es humillar a los perros, sino más bien promover la adopción de mascotas sin importar su aspecto.</p>
+							
+						  </div>
+						</div>
+					  </div>
+					</div>
 						
 			
 				</div>
@@ -37,7 +109,7 @@ const Miguel = () => {
 					<h2>Lorem Ipsum</h2>
 					<div className="body-ejercicio1">
 						<img src="https://dummyimage.com/300x300/e3a6d1/fff" alt="image 1"/>
-						<p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. </p>
+						<p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classNameical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classNameical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. </p>
 					</div>
 
 				</div>
