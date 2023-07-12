6881e5366ac648a57a9c95f71781aa6dfbc27587
GuillermoACM <GuillermoA.ContrerasM@gmail.com>
2023-07-12 15:36:13 -0400

    Albani design

--- src/container/Albani/Albani.jsx
+++ src/container/Albani/Albani.jsx
@@ -21,25 +21,28 @@ const Albani = () => {
 						
 					</div>
 					
-					<div className="container-bio">
+					<div className="container-bio">						
 					
-						<p className="about-bio"></p>
-						
 						<div className="container-skills">
 						
 							<ul className="about-skills">
 								<li>
-									<img src="https://via.placeholder.com/100x100" />
+										<img src="https://via.placeholder.com/100x100" />
+										<span>Habilidad</span>
 								</li>
 								<li>
 									<img src="https://via.placeholder.com/100x100" />
+									<span>Habilidad</span>
 								</li>
 								<li>
 									<img src="https://via.placeholder.com/100x100" />
+									<span>Habilidad</span>
 								</li>
 							</ul>
 							
 						</div>
+					
+						<p className="about-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat vitae felis quis lobortis. Praesent blandit elit id tempus feugiat. Aliquam lobortis diam quam, eget scelerisque mauris posuere quis. Duis maximus orci at massa dictum consectetur. Duis et quam facilisis, dapibus nibh eget, viverra velit. Nullam nibh augue, porta venenatis mi vitae, sodales molestie quam. Nam et orci lacus. </p>
 						
 					</div>
 					
